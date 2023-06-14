f699bf85c408d1b19652bf574a666b3f99419365
Connor Vosberg <connorjv@gmail.com>
2023-06-14 13:39:52 -0700

    adds a get shlters/id into appplication controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,24 +2,36 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/test" do
-    { message: "Good luck with your project!" }.to_json
-  end
-
   get "/shelters" do
     shelters = Shelter.all
     shelters.to_json
   end
+
+  get "/shelters/:id" do
+    shelter = Shelter.find(params[:id])
+    shelter.to_json
+  end
+
+
 
   get "/breeders" do
     breeders = Breeder.all
     breeders.to_json
   end
 
+
+
   get "/dogs" do
     dogs = Dog.all
     dogs.to_json
   end
+
+  get "/dogs/:id" do
+    dog = Dog.find(params[:id])
+    dog.to_json
+  end
+
+  
 
   post "/dogs" do
     dog = Dog.create(
@@ -43,6 +55,8 @@ class ApplicationController < Sinatra::Base
     dog.destroy
     dog.to_json
   end
+
+
 
   get "/cats" do
     cats = Cat.all
--- app/models/Shelter.rb
+++ app/models/Shelter.rb
@@ -1,4 +1,8 @@
 class Shelter < ActiveRecord::Base
     has_many :dogs
     has_many :cats
+
+    def allDogs 
+        self.dogs.all
+    end
 end
